15d70c03deb8e465d191d27c45a8d21fcf8801bb
Menna <mennamokhtar621@gmail.com>
2026-04-15 18:53:10 +0200

    Add files via upload

---
+++ Sessions/Session14(js)/script.js
@@ -0,0 +1,79 @@
+console.warn("errorrrrrrrrr")
+
+var user1 = "Menna";
+
+var nums = 22;
+
+console.log(user1 , typeof nums)
+
+var user2 = "Tala";
+user2 ="Adam";
+
+console.log(user2);
+
+const user3 = "Yazed";
+
+// user3 = "Menna";  not true
+
+console.log((user3))
+
+let IsMarried = true;
+console.log(IsMarried , typeof IsMarried);
+
+let fname;
+let lname = null;
+let obj = {};
+let arr= [];
+let num0 = 9999999999999999;
+let num1 = 9999999999999999n;
+console.log(fname , typeof fname)
+console.log(lname , typeof lname)
+console.log(obj , typeof obj)
+console.log(arr , typeof arr)
+console.log(Array.isArray(arr))
+console.log(num0 , typeof(num0))
+console.log(num1 , typeof(num1))
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
+
+let num2=60.35;
+let num3=55;
+let num4=999.7;
+
+console.log( num2.toString() ,typeof num2.toString())
+console.log( num2.toFixed() ,typeof num2.toFixed())
+console.log( num2.toFixed() ,typeof num2.toFixed(0))
+
+console.log( Number.isInteger(num2))
+console.log( Number.isInteger(num3))
+
+console.log(Math.round(num2) , typeof num2)
+console.log(Math.ceil(num2))
+console.log(Math.floor(num2))
+console.log(Math.trunc(num4))
+
+console.log(Math.max(num3,num2,num4))
+console.log(Math.min(num3,num2,num4))
+
+console.log(Math.random())
+console.log(Math.random())
+
+console.log(Math.trunc(Math.random()*11))
+console.log(Math.round(Math.random()*11))
+console.log(Math.trunc(Math.random()*11))
+
+console.log(Math.sqrt(81))
+console.log(Math.sqrt(16))
+console.log(Math.sqrt(49))
+console.log(Math.abs(-90))
+console.log(Math.pow(2,3))
+
+
+let n=70.4;
+
+console.log(n.toFixed())
+console.log(Math.floor(n))
+console.log(Math.round(n))
+console.log(Math.trunc(n))
